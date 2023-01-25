96878921ceab5a85dcf7f6d1b749c06036354979
Midland707 <rus14job@ukr.net>
2023-01-25 22:44:32 +0200

    add

--- src/partials_js/search.js
+++ src/partials_js/search.js
@@ -1,8 +1,24 @@
+// autor: Ruslan branch Search_FT-10
+
 import { Movie } from './api';
 import { markup } from './markup_service';
 
-const movie = new Movie({
-  // searchValue: '',
-});
+let search = '';
+const searchValue = document.querySelector('search-form-input');
+const searchButton = document.querySelector('.search-form');
 
-console.log(movie.fetchSearchMovies());
+searchValue.addEventListener('input', onInput);
+function onInput() {
+  search = searchValue.value;
+}
+
+searchButton.addEventListener('submit', onSubmit);
+async function onSubmit(event) {
+    event.preventDefault();
+    const movie = new Movie({
+        searchValue: search,
+    });
+    
+    console.log(movie.fetchSearchMovies());
+    // markup();
+}
