05ce36dce88c70902c8644b72014d575e5b4ecf2
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-01-25 23:46:23 +0200

    change the comment to the function

--- src/partials_js/api.js
+++ src/partials_js/api.js
@@ -6,105 +6,106 @@ const API_URL_SEARCH_MOVIE = 'https://api.themoviedb.org/3/search/movie';
 const API_URL_MOVIE_DETAILS = 'https://api.themoviedb.org/3/movie/';
 
 class Movie {
-	constructor({ searchValue }) {
-		this.searchValue = searchValue;
-		this.currentPage = 1;
-	}
+  constructor({ searchValue }) {
+    this.searchValue = searchValue;
+    this.currentPage = 1;
+  }
 
-	/**
-	 *
-	 * @returns a list of popular movies for today
-	 */
-	async fetchTrendingMovies() {
-		try {
-			const response = await axios.get(API_URL_TRENDING_MOVIE, {
-				params: {
-					api_key: API_KEY,
-					page: this.currentPage,
-				}
-			});
+  /**
+   *
+   * @returns a list of popular movies for today
+   */
+  async fetchTrendingMovies() {
+    try {
+      const response = await axios.get(API_URL_TRENDING_MOVIE, {
+        params: {
+          api_key: API_KEY,
+          page: this.currentPage,
+        }
+      });
 
-			console.log(response.data);
-			console.log('Total Pages:', response.data.total_pages);
+      console.log(response.data);
+      console.log('Total Pages:', response.data.total_pages);
 
-			return response.data;
-		} catch (error) {
-			console.error(error);
-		}
-	}
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-	/**
-	 *
-	 * @returns a list of movies by keyword
-	 */
-	async fetchSearchMovies() {
-		try {
-			const response = await axios.get(API_URL_SEARCH_MOVIE, {
-				params: {
-					api_key: API_KEY,
-					query: this.searchValue,
-					language: 'en-US',
-					page: this.currentPage,
-					include_adult: 'false,'
-				}
-			});
+  /**
+   *
+   * @returns a list of movies by keyword
+   */
+  async fetchSearchMovies() {
+    try {
+      const response = await axios.get(API_URL_SEARCH_MOVIE, {
+        params: {
+          api_key: API_KEY,
+          query: this.searchValue,
+          language: 'en-US',
+          page: this.currentPage,
+          include_adult: 'false,'
+        }
+      });
 
-			console.log(response.data);
+      console.log(response.data);
 
-			return response.data;
-		} catch (error) {
-			console.error(error);
-		}
-	}
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-	/**
-	   * @param {API_URL_MOVIE_DETAILS} id
-	   * @returns full information about the movie
-	   */
-	async fetchMovieDetails(id = `39860`) {
-		try {
-			const response = await axios.get(`${API_URL_MOVIE_DETAILS}${id}`, {
-				params: {
-					api_key: API_KEY,
-					language: 'en-US',
-				}
-			})
+  /**
+     * @param {String} API_URL_MOVIE_DETAILS
+     * @param {String} id
+     * @returns full information about the movie
+     */
+  async fetchMovieDetails(id = `39860`) {
+    try {
+      const response = await axios.get(`${API_URL_MOVIE_DETAILS}${id}`, {
+        params: {
+          api_key: API_KEY,
+          language: 'en-US',
+        }
+      })
 
-			console.log(response.data);
+      console.log(response.data);
 
-			return response.data;
-		} catch (error) {
-			console.error(error);
-		}
-	}
-	/**
-	 *
-	 * @param {*} id
-	 * @returns full information about a possible trailer
-	 */
-	async fetchMovieVideo(id = `39860`) {
-		try {
-			const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos`, {
-				params: {
-					api_key: API_KEY,
-				}
-			})
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  /**
+   *
+   * @param {String} id
+   * @returns full information about a possible trailer
+   */
+  async fetchMovieVideo(id = `39860`) {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos`, {
+        params: {
+          api_key: API_KEY,
+        }
+      })
 
-			console.log(response.data);
+      console.log(response.data);
 
-			return response.data;
-		} catch (error) {
-			console.error(error);
-		}
-	}
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-	nextPage() {
-		this.currentPage += 1;
-	}
+  nextPage() {
+    this.currentPage += 1;
+  }
 
-	resetPage() {
-		this.currentPage = 1;
-	}
+  resetPage() {
+    this.currentPage = 1;
+  }
 }
 
 export { Movie };
