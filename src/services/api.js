e6220d30936c91fbd098a418051e657e0129aac2
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-01-25 12:56:32 +0200

    fixed backend requests

--- src/services/api.js
+++ src/services/api.js
@@ -3,93 +3,95 @@ import axios from "axios";
 const API_KEY = 'fed7db976d902fcdece547680e82ff9e';
 const API_URL_TRENDING_MOVIE = 'https://api.themoviedb.org/3/trending/movie/day';
 const API_URL_SEARCH_MOVIE = 'https://api.themoviedb.org/3/search/movie';
-
-
-// * По документации
-// const API_URL_MOVIE_DETAILS = 'https://api.themoviedb.org/3/movie/{movie_id}';
-
-// /movie/{movie_id} - integer
-// {movie_id}, я так поняла, тянуть с localStorage (когда первые 20 загрузились)
-
-const API_URL_MOVIE_DETAILS = `https://api.themoviedb.org/3/movie/
-536554`;//так находит
-
-// * По документации Та же проблема
-const API_URL_MOVIE_VIDEO = 'https://api.themoviedb.org/3/movie/{movie_id}/videos';
-
+const API_URL_MOVIE_DETAILS = 'https://api.themoviedb.org/3/movie/';
 
 class Movie {
   constructor({ searchValue }) {
     this.searchValue = searchValue;
     this.currentPage = 1;
-
-    // поиск по ключевому слову
-    this.currentQuery = 'cat';
   }
 
+  /**
+   *
+   * @returns a list of popular movies for today
+   */
   async fetchTrendingMovies() {
     try {
       const response = await axios.get(API_URL_TRENDING_MOVIE, {
         params: {
           api_key: API_KEY,
+          page: this.currentPage,
         }
       });
+
       console.log(response.data);
+      console.log('Total Pages:', response.data.total_pages);
+
       return response.data;
     } catch (error) {
       console.error(error);
     }
   }
 
+  /**
+   *
+   * @returns a list of movies by keyword
+   */
   async fetchSearchMovies() {
     try {
       const response = await axios.get(API_URL_SEARCH_MOVIE, {
         params: {
           api_key: API_KEY,
-          query: this.currentQuery,
+          query: 'cat', //change on 'this.searchValue'
           language: 'en-US',
           page: this.currentPage,
           include_adult: 'false,'
         }
       });
+
       console.log(response.data);
+
       return response.data;
     } catch (error) {
       console.error(error);
     }
   }
 
-  async fetchMovieDetails() {
+  /**
+     * @param {API_URL_MOVIE_DETAILS} id
+     * @returns full information about the movie
+     */
+  async fetchMovieDetails(id = `39860`) {
     try {
-      // * попытка
-      // const response = await axios.get(`API_URL_MOVIE_DETAILS({ results }).map(({ id }) => id), {
-      //   params: {
-      //     api_key: API_KEY,
-      //     language: 'en-US',
-      //   }
-      // }`)
-      const response = await axios.get(API_URL_MOVIE_DETAILS, {
+      const response = await axios.get(`${API_URL_MOVIE_DETAILS}${id}`, {
         params: {
           api_key: API_KEY,
           language: 'en-US',
         }
       })
+
       console.log(response.data);
+
       return response.data;
     } catch (error) {
       console.error(error);
     }
   }
-
-  async fetchMovieVideo() {
+  /**
+   *
+   * @param {*} id
+   * @returns full information about a possible trailer
+   */
+  async fetchMovieVideo(id = `39860`) {
     try {
-      const response = await axios.get(API_URL_MOVIE_VIDEO, {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos`, {
         params: {
           api_key: API_KEY,
-          language: 'en-US',
         }
       })
+
       console.log(response.data);
+
       return response.data;
     } catch (error) {
       console.error(error);
@@ -106,4 +108,3 @@ class Movie {
 }
 
 export { Movie };
-
