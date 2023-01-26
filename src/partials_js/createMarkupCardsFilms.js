4f6644335e1279f8f2054185432b56ce5a12b69a
OPaslavskiy <bankir.op@gmail.com>
2023-01-26 02:54:08 +0200

    no photo no

--- src/partials_js/createMarkupCardsFilms.js
+++ src/partials_js/createMarkupCardsFilms.js
@@ -1,24 +1,36 @@
 import { Movie } from './api';
 
 export default function createMarkupCardsFilms(arrayMovies) {
+  const BASE_URL_POSTER = 'https://image.tmdb.org/t/p/w500';
+  // const imgRow = arrayMovies.map({ poster_path })
+  //   ? `<img src="${BASE_URL_POSTER}${poster_path}" width="300px" height="450px" />`
+  //   : `fu7ck`;
+  // console.log(arrayMovies);
   return (markup = arrayMovies
-    .map(
-      ({
-        poster_path,
-        title,
-        genre_ids,
-        release_date,
-        vote_average,
-      }) => `<li class="item-films">
-              <img src="https://image.tmdb.org/t/p/w500${poster_path}" />
-              <p class="title">${title}</p>
+    .map(({ poster_paths, title, genre_ids, release_date, vote_average }) => {
+      const imgRow = poster_paths
+        ? `<img src="${BASE_URL_POSTER}${poster_path}" />`
+        : // : `<img src="../images/no_photo.jpg" alt="xxx">`;
+          `<img src="../images/no_photo.jpg" alt="xxx">`;
+
+      return `<li class="item-films">
+              ${imgRow}
+              <p class="title">${title ? title : 'Sorry, no information'} </p>
               <div class="film-info">
-                <p class="genre">${genre_ids},</p>
-                <p class="year">${release_date}</p>
-                <p class="rating">${vote_average}</p>
+                <p class="genre">${
+                  genre_ids ? genre_ids : 'Sorry, no information'
+                },</p>
+                <p class="year">${
+                  release_date
+                    ? release_date.slice(0, 4)
+                    : 'Sorry, no information'
+                }</p>
+                <p class="rating">${
+                  vote_average ? vote_average : 'Sorry, no information'
+                }</p>
               </div>
-            </li>`
-    )
+            </li>`;
+    })
     .join(''));
 }
 
