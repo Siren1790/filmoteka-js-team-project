09fd1334e651936ead64909ad6a36df1eb6d0c75
Sergii Karpus <49605599+Siren1790@users.noreply.github.com>
2023-01-27 01:12:35 +0200

    fixed the mistakes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { Movie } from './partials_js/api';
 import { Genres } from './partials_js/genres';
+import createMarkupCardsFilms from './partials_js/createMarkupCardsFilms';
+
+const mainMarkFilms = document.querySelector('.list-films');
 
 const movie = new Movie({
   // searchValue: '',
@@ -9,5 +12,8 @@ movie.fetchTrendingMovies();
 movie.fetchSearchMovies();
 movie.fetchMovieDetails();
 movie.fetchMovieVideo();
-movie.fetchMovieVideo();
 movie.fetchMovieGenres();
+
+movie.fetchTrendingMovies().then(data => {
+  mainMarkFilms.innerHTML = createMarkupCardsFilms(data.results);
+});
