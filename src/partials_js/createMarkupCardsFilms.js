e9768977900a28bed3c4b52eca8570acd670d812
Sergii Karpus <49605599+Siren1790@users.noreply.github.com>
2023-01-27 10:05:18 +0200

    review code

--- src/partials_js/createMarkupCardsFilms.js
+++ src/partials_js/createMarkupCardsFilms.js
@@ -1,30 +1,20 @@
+const BASE_URL_POSTER = 'https://image.tmdb.org/t/p/w500';
 
 export default function createMarkupCardsFilms(arrayMovies) {
-  const BASE_URL_POSTER = 'https://image.tmdb.org/t/p/w500';
-
-  return (markup = arrayMovies
+  let markup = arrayMovies
     .map(({ poster_path, title, genre_ids, release_date, vote_average }) => {
-      const imgRow = poster_path
-        ? `<img src="${BASE_URL_POSTER}${poster_path}" />`
-        : `<img src="./images/no_image.jpg" alt="no photo" width="400" height="500">`;
-
+      const imgRow = !poster_path ? `<img src="${BASE_URL_POSTER}${poster_path}" />` : `<img src="./images/no_image.jpg" alt="no photo" width="400" height="500">`;
       return `<li class="item-films">
-              ${imgRow}
-              <p class="title">${title ? title : 'Sorry, no information'} </p>
-              <div class="film-info">
-                <p class="genre">${
-                  genre_ids ? genre_ids : 'Sorry, no information'
-                },</p>
-                <p class="year">${
-                  release_date
-                    ? release_date.slice(0, 4)
-                    : 'Sorry, no information'
-                }</p>
-                <p class="rating">${
-                  vote_average ? vote_average : 'Sorry, no information'
-                }</p>
-              </div>
-            </li>`;
+                ${imgRow}
+                <p class="title">${title ? title : 'Sorry, no information'} </p>
+                <div class="film-info">
+                  <p class="genre">${genre_ids ? genre_ids : 'Sorry, no information'},</p>
+                  <p class="year">${release_date ? release_date.slice(0, 4) : 'Sorry, no information'}</p>
+                  <p class="rating">${vote_average ? vote_average : 'Sorry, no information'}</p>
+                </div>
+              </li>`;
     })
-    .join(''));
+    .join('');
+
+    return markup;
 }
