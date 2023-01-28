248852a8e2070cad5257408f02485e3317310bca
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2023-01-28 03:31:42 +0200

    fix backdrop

--- src/partials_js/modal_movies.js
+++ src/partials_js/modal_movies.js
@@ -7,4 +7,15 @@ heartOn.addEventListener(
   () => (heartOn.style.backgroundColor = '#f8d792e5')
 );
 
-// modal
+// modal - do later
+
+// const closeModalOnButtonClick = document.querySelector('#close-button');
+
+// closeModalOnButtonClick.addEventListener('click', closeBtnModalOnClick);
+
+// function closeBtnModalOnClick(event) {
+//   event.preventDefault();
+//   let backdrop = document.querySelector('backdrop-modal');
+
+//   event.currentTarget.classList.add('.visually-hidden');
+// }
