cdfd90d334b0b68c02233efb5c35f70f4cc0ae8e
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2023-01-28 05:00:18 +0200

    in progress-1

---
+++ src/partials_js/our-team.js
@@ -0,0 +1,67 @@
+import * as basicLightbox from 'basiclightbox';
+import catharinaUrl from '../images/catharina.jpeg';
+import glebUrl from '../images/gleb.jpeg';
+import hannaaUrl from '../images/hanna.jpeg';
+import olegUrl from '../images/oleg.jpeg';
+import spartakUrl from '../images/spartak.jpeg';
+import spriteUrl from '../images/sprite.svg';
+
+const markup = `<div class="team-container"><div class="team-card">
+    <img src="${catharinaUrl}" alt="Catharina" class="team-image">
+    <p class="team-name">Catharina</p>
+    <p class="team-role">Developer</p>
+    <a href="#" target="_blank" class="git-link"><svg class="git-logo" width="20" height="20">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${glebUrl}" alt="Gleb" class="team-image">
+    <p class="team-name">Gleb</p>
+    <p class="team-role">Developer</p>
+    <a href="#" target="_blank" class="git-link"><svg class="git-logo" width="20" height="20">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${hannaaUrl}" alt="Hanna" class="team-image">
+    <p class="team-name">Hanna</p>
+    <p class="team-role">Developer</p>
+    <a href="#" target="_blank" class="git-link"><svg class="git-logo" width="20" height="20">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${olegUrl}" alt="Oleg" class="team-image">
+    <p class="team-name">Oleg</p>
+    <p class="team-role">Developer</p>
+    <a href="#" target="_blank" class="git-link"><svg class="git-logo" width="20" height="20">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${spartakUrl}" alt="Spartak" class="team-image">
+    <p class="team-name">Spartak</p>
+    <p class="team-role">Developer</p>
+    <a href="#" target="_blank" class="git-link"><svg class="git-logo" width="20" height="20">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+</div></div>`;
+const container = document.querySelector('.js-our-team');
+
+container.addEventListener('click', openModal);
+
+const modal = basicLightbox.create(markup);
+
+function openModal(e) {
+  modal.show();
+
+  window.addEventListener('keydown', closeModalHandler);
+
+  function closeModalHandler(e) {
+    if (e.code === 'Escape') {
+      modal.close();
+      window.removeEventListener('keydown', closeModalHandler);
+    }
+  }
+}
