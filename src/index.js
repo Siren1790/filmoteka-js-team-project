83cfe947e4055a3d2c98836248604b0b04d7d999
OPaslavskiy <bankir.op@gmail.com>
2023-01-29 00:04:00 +0200

    fix first local storeg genres

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import { Movie } from './partials_js/api';
-import { Genres } from './partials_js/genres';
 import createMarkupCardsFilms from './partials_js/createMarkupCardsFilms';
-import {
-  saveStorageGenres,
-  createGenresObject,
-} from './partials_js/local_genres-storage';
+import { genresCreate } from './partials_js/local_genres-storage';
 
 const mainMarkFilms = document.querySelector('.list-films');
 
@@ -12,11 +8,7 @@ const movie = new Movie({
   searchValue: '',
 });
 
-movie
-  .fetchMovieGenres()
-  .then(createGenresObject)
-  .then(saveStorageGenres)
-  .catch();
+genresCreate();
 
 movie.fetchTrendingMovies().then(data => {
   mainMarkFilms.innerHTML = createMarkupCardsFilms(data.results);
--- src/partials_js/createMarkupCardsFilms.js
+++ src/partials_js/createMarkupCardsFilms.js
@@ -1,14 +1,5 @@
-import { Movie } from './api';
-import {
-  markUpMainGenres,
-  createGenresObject,
-  saveStorageGenres,
-} from './local_genres-storage';
+import { markUpMainGenres, genresCreate } from './local_genres-storage';
 const BASE_URL_POSTER = 'https://image.tmdb.org/t/p/w500';
-
-const movie = new Movie({
-  searchValue: '',
-});
 
 /**
  *
@@ -19,24 +10,18 @@ export default function createMarkupCardsFilms(arrayMovies) {
   let markup = arrayMovies
     .map(({ poster_path, title, genre_ids, release_date, vote_average }) => {
       let genresState = localStorage.getItem('genres');
-      let genresLoad;
+      const loadGenres = JSON.parse(genresState);
+
       if (!localStorage.genres) {
-        movie
-          .fetchMovieGenres()
-          .then(createGenresObject)
-          .then(saveStorageGenres)
-          .catch();
+        genresCreate();
         genresState = localStorage.getItem('genres');
-        const loadGenres = JSON.parse(genresState); // array id(genres): genres(value)
-        genresLoad = markUpMainGenres(genre_ids, loadGenres);
-      } else {
-        const loadGenres = JSON.parse(genresState); // array id(genres): genres(value)
-        genresLoad = markUpMainGenres(genre_ids, loadGenres);
       }
 
+      let genresLoad = markUpMainGenres(genre_ids, loadGenres);
+
       const imgRow = poster_path
-        ? `<img src="${BASE_URL_POSTER}${poster_path}" />`
-        : `<img src="./images/no_image.jpg" alt="no photo" width="400" height="500">`;
+        ? `<img class="img-cover" src="${BASE_URL_POSTER}${poster_path}" />`
+        : `<img class="img-cover" src="./images/no_image.jpg" alt="no photo" width="400" height="500">`;
 
       return `<li class="item-films">
                 ${imgRow}
