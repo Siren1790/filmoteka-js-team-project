d79b465e9b0ca6b91ad56f71afdfada67bb76a23
YaroslavYu <yaroslavyu801@gmail.com>
2023-01-31 22:07:29 +0200

    add dynamic data in modal

--- src/partials_js/data-for-modal.js
+++ src/partials_js/data-for-modal.js
@@ -1,66 +1,95 @@
 const BASE_URL_POSTER = 'https://image.tmdb.org/t/p/w500';
 const genres = JSON.parse(localStorage.getItem('genres'));
+import axios from 'axios';
+import { refs, refsApi, refsStorage } from './refs';
 
-const divCard = document.querySelector('.list-films');
+// const divCard = document.querySelector('.list-films');
 
-divCard.addEventListener('click', restDataForModal);
+// refs.mainMarkFilms.addEventListener('click', restDataForModal);
 
 function restDataForModal(e) {
-  if (e.currentTarget === e.target) return;
+  // if (e.currentTarget === e.target) return;
+
+  // console.log('err');
 
   const targetClickMovieCard = e.target.closest('.item-films');
   const currentMoviesFromStorage = JSON.parse(
-    localStorage.getItem('currentMovies')
+    localStorage.getItem('currentFilms')
   );
-  const titleMoviesFromClick = targetClickMovieCard
-    .querySelector('.title')
-    .textContent.trim();
+
+  // console.dir(targetClickMovieCard);
+  const movieIdFromClick = targetClickMovieCard.dataset.indexFilm;
+  // .textContent.trim();
+
+  // console.log(currentMoviesFromStorage);
 
   const indexOfMovie = currentMoviesFromStorage.findIndex(
-    movieObj => movieObj.title === titleMoviesFromClick
+    movieObj => movieObj.id == movieIdFromClick
   );
+  // console.log(currentMoviesFromStorage[indexOfMovie]);
 
-  const currentMovie = currentMoviesFromStorage[indexOfMovie];
+  return currentMoviesFromStorage[indexOfMovie];
+  // const currentMovie = currentMoviesFromStorage[indexOfMovie];
 
-  dataObjForModal = createDataObjectForModal(currentMovie);
-  console.log(dataObjForModal);
+  // dataObjForModal = createDataObjectForModal(currentMovie);
+  // console.log(dataObjForModal);
 }
 
 function createStringOfGenres(arrayCodesOfGenres) {
-  const arrayStringGenresMovie = arrayCodesOfGenres.reduce(
-    (arrayStringsGenres, keyOfGenre) => {
-      arrayStringsGenres.push(genres[keyOfGenre]);
-      return arrayStringsGenres;
-    },
-    []
-  );
-  return arrayStringGenresMovie.join(', ');
+  return arrayCodesOfGenres.reduce((stringGenres, keyOfGenre) => {
+    if (!stringGenres) {
+      return genres[keyOfGenre];
+    }
+    return stringGenres + ', ' + genres[keyOfGenre];
+  }, '');
 }
 
-function createDataObjectForModal(fullObjectMovie) {
-  const {
-    poster_path,
-    title,
-    original_title,
-    vote_average,
-    vote_count,
-    popularity,
-    genre_ids,
-    overview,
-  } = fullObjectMovie;
+// function createDataObjectForModal(fullObjectMovie) {
+//   const {
+//     // poster_path,
+//     // title,
+//     original_title,
+//     vote_average,
+//     vote_count,
+//     popularity,
+//     genre_ids,
+//     overview,
+//   } = fullObjectMovie;
 
-  const dataObjForModal = {
-    poster: BASE_URL_POSTER + poster_path,
-    title,
-    original_title,
-    rating: vote_average,
-    votes: vote_count,
-    popularity,
-    genres: createStringOfGenres(genre_ids),
-    about: overview,
-  };
+//   const dataObjForModal = {
+//     poster: BASE_URL_POSTER + poster_path,
+//     title,
+//     original_title,
+//     rating: vote_average,
+//     votes: vote_count,
+//     popularity,
+//     genres: createStringOfGenres(genre_ids),
+//     about: overview,
+//   };
 
-  return dataObjForModal;
+//   return dataObjForModal;
+// }
+
+export { restDataForModal, createStringOfGenres };
+
+async function fetchMovieVideo(id = 505642) {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}/videos`,
+      {
+        params: {
+          api_key: refsApi.API_KEY,
+        },
+      }
+    );
+
+    console.log(response.data);
+    console.log('Trailer Response Results', response.data.results);
+    console.log('Trailer Response Results Key', response.data.results[0].key);
+    return response.data.results[0].key;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export { restDataForModal };
+// fetchMovieVideo();
