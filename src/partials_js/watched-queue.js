d3e4abe9d5c85b3f09e5ad0fb67e06d0e0124d60
OPaslavskiy <bankir.op@gmail.com>
2023-02-01 23:35:44 +0200

    all reday whith library (only need change LocalStorage)

--- src/partials_js/watched-queue.js
+++ src/partials_js/watched-queue.js
@@ -4,7 +4,7 @@ import { refs, refsStorage } from './refs';
 const queueBtn = document.querySelectorAll('.header-btn');
 
 const emptyLibraryAnimation = document.querySelector('.library__img-container');
-// const emptyLibraryParagraoh = document.querySelector('.empty-paragraph');
+const emptyLibraryParagraoh = document.querySelector('.empty-paragraph');
 const newWatchedBtn = queueBtn[0];
 const newQueueBtn = queueBtn[1];
 
@@ -27,29 +27,47 @@ function hiddenEmptyAnimation() {
     localStorage.getItem(refsStorage.STORAGE_KEY_QUEUE)
   ) {
     emptyLibraryAnimation.classList.add('visually-hidden');
-    // emptyLibraryParagraoh.classList.add('visually-hidden');
+    emptyLibraryParagraoh.classList.add('visually-hidden');
     liberyDrawings();
   } else return;
 }
 
 function onClickMakeMarkUpWatched() {
-  emptyLibraryAnimation.classList.add('visually-hidden');
-  // emptyLibraryParagraoh.classList.add('visually-hidden');
-  const arrayFilmsWatched = JSON.parse(
-    localStorage.getItem(refsStorage.CURRENT_FILMS)
-  ).results;
-  newWatchedBtn.classList.add('header-btn-active');
-  let markup = createMarkupCardsFilms(arrayFilmsWatched);
-  refs.mainMarkFilms.innerHTML = markup;
+  if (localStorage.getItem(refsStorage.STORAGE_KEY_WATCHED)) {
+    emptyLibraryAnimation.classList.add('visually-hidden');
+    emptyLibraryParagraoh.classList.add('visually-hidden');
+    newQueueBtn.classList.remove('header-btn-active');
+    const arrayFilmsWatched = JSON.parse(
+      localStorage.getItem(refsStorage.CURRENT_FILMS)
+    ).results;
+    newWatchedBtn.classList.add('header-btn-active');
+    let markup = createMarkupCardsFilms(arrayFilmsWatched);
+    refs.mainMarkFilms.innerHTML = markup;
+  } else {
+    newQueueBtn.classList.remove('header-btn-active');
+    newWatchedBtn.classList.add('header-btn-active');
+    emptyLibraryAnimation.classList.remove('visually-hidden');
+    emptyLibraryParagraoh.classList.remove('visually-hidden');
+    refs.mainMarkFilms.innerHTML = '';
+  }
 }
 
 function onClickMakeMarkUpQueue() {
-  emptyLibraryAnimation.classList.add('visually-hidden');
-  // emptyLibraryParagraoh.classList.add('visually-hidden');
-  const arrayFilmsWatched = JSON.parse(
-    localStorage.getItem(refsStorage.CURRENT_FILMS)
-  ).results;
-  newQueueBtn.classList.add('header-btn-active');
-  let markup = createMarkupCardsFilms(arrayFilmsWatched);
-  refs.mainMarkFilms.innerHTML = markup;
+  if (localStorage.getItem(refsStorage.STORAGE_KEY_QUEUE)) {
+    emptyLibraryAnimation.classList.add('visually-hidden');
+    emptyLibraryParagraoh.classList.add('visually-hidden');
+    newWatchedBtn.classList.remove('header-btn-active');
+    newQueueBtn.classList.add('header-btn-active');
+    const arrayFilmsWatched = JSON.parse(
+      localStorage.getItem(refsStorage.CURRENT_FILMS)
+    ).results;
+    let markup = createMarkupCardsFilms(arrayFilmsWatched);
+    refs.mainMarkFilms.innerHTML = markup;
+  } else {
+    newWatchedBtn.classList.remove('header-btn-active');
+    newQueueBtn.classList.add('header-btn-active');
+    emptyLibraryAnimation.classList.remove('visually-hidden');
+    emptyLibraryParagraoh.classList.remove('visually-hidden');
+    refs.mainMarkFilms.innerHTML = '';
+  }
 }
