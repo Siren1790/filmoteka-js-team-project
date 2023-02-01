1d59967664d44ce35802a65f2c6ce1393638c69c
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2023-02-01 04:05:58 +0200

    adjustable styles hight

--- src/partials_js/modal_movies.js
+++ src/partials_js/modal_movies.js
@@ -28,7 +28,7 @@ const markup = `    <div class="movie-card" id="bright">
     
             <div class="info-section">
                 <div class="title-wrapper">
-                    <h1 class="card-movie-title">Brightffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</h1>
+                    <h1 class="card-movie-title">Bright</h1>
                 </div>
     
                 <table class="card-movie-info">
@@ -43,7 +43,7 @@ const markup = `    <div class="movie-card" id="bright">
                     <tr class="row">
                         <td class="row-title">Original Title</td>
                         <td class="row-value row-title-original">
-                            fffffffff fggggg gggsssss ssssss ssssssss ssssss ssssssss sssggggg gfffffffffff fffff ffff fffff fffff fffff fff</td>
+                            fffffffff  fff</td>
                     </tr>
                     <tr class="row">
                         <td class="row-title">Genre</td>
@@ -64,24 +64,6 @@ const markup = `    <div class="movie-card" id="bright">
                     <li><button type="button" class="queue-btn js-btn-queue">Add to Queue</button></li>
                 </ul>
     
-                <div class="movie-social">
-                    <ul class="social-icons">
-                        <li>
-                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-share" viewBox="0 0 16 16">
-                                <path
-                                    d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
-                            </svg>
-                        </li>
-                        <li class="heart-box">
-                            <svg id="heart" class="icon icon-heart" xmlns="http://www.w3.org/2000/svg"
-                                width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                <path
-                                    d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                            </svg>
-                        </li>
-                    </ul>
-                </div>
                 </div>
             </div>
         </div>
