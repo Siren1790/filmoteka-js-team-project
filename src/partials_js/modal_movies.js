5e0716fb2f4de893f059b4704632d15cfe42e6f7
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2023-02-01 05:44:14 +0200

    merged js modal files

--- src/partials_js/modal_movies.js
+++ src/partials_js/modal_movies.js
@@ -21,66 +21,65 @@ function createMarkupModal(objMovieInfo) {
     overview,
   } = objMovieInfo;
 
-  const markup = `<div class="movie_card" id="bright">
+  const markup = `<div class="movie-card" id="bright">
+        <div class="button-wrapper">
             <div class="button-container">
                 <button class="close-button" id='close-button'>Close</button>
             </div>
             <div class="button-container">
                 <button class="watch-trailer js-watch-trailer" id='watch-trailer'>Watch Trailer</button>
             </div>
-            <img class="card__main-poster"
-                src="${BASE_URL_POSTER}${poster_path}" />
-            <div class="info_section">
-        
-                <h1 class="card__movie-title title">${title}</h1>
-        
-                <table class="card__movie-info">
+        </div>
+    
+        <div class="info-wrapper">
+    
+            <div class="#">
+                <img class="card-main-poster"
+                    src="${BASE_URL_POSTER}${poster_path}" />
+            </div>
+    
+            <div class="info-section">
+                <div class="title-wrapper">
+                    <h1 class="card-movie-title">${title}</h1>
+                </div>
+    
+                <table class="card-movie-info">
                     <tr class="row">
                         <td class="row-title">Vote / Votes</td>
-                        <td><span class="votes">${vote_average}</span> / ${vote_count}</td>
+                        <td class="row-value"><span class="votes">${vote_average}</span> / ${vote_count}</td>
                     </tr>
                     <tr class="row">
                         <td class="row-title">Popularity</td>
-                        <td>${popularity}</td>
+                        <td class="row-value">${popularity}</td>
                     </tr>
                     <tr class="row">
                         <td class="row-title">Original Title</td>
-                        <td>${original_title}</td>
+                        <td class="row-value row-title-original">${original_title}</td>
                     </tr>
                     <tr class="row">
                         <td class="row-title">Genre</td>
-                        <td>${createStringOfGenres(genre_ids)}</td>
+    <td class="row-value genres-row">${createStringOfGenres(genre_ids)}</td>
                     </tr>
                 </table>
-        
-                <div class="movie_descr">
+    
+                <div class="movie-descr">
                     <p class="movie-descr__about">About</p>
-                    <p class="movie-descr__text">${overview}
+                    <p class="movie-descr__text">
+                        ${overview}
                     </p>
                 </div>
-        
+    
+                <div class="desktop-wrapper">
                 <ul class="modal-buttons">
-                    <li><button type="button" class="watch-btn">Add to Watched</button></li>
-                    <li><button type="button" class="queue-btn">Add to queue</button></li>
+                    <li><button type="button" class="watch-btn js-btn-watched">Add to Watched</button></li>
+                    <li><button type="button" class="queue-btn js-btn-queue">Add to Queue</button></li>
                 </ul>
-        
-                <div class="movie_social">
-                    <ul class="social-icons">
-                        <li><svg class="icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-share" viewBox="0 0 16 16">
-                                <path
-                                    d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
-                            </svg></li>
-                        <li class="heart-box"><svg id="heart" class="icon icon-heart" xmlns="http://www.w3.org/2000/svg"
-                                width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                <path
-                                    d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                            </svg></li>
-                    </ul>
+    
                 </div>
             </div>
-
-        </div>`;
+        </div>
+    
+    </div>`;
 
   return markup;
 }
@@ -94,9 +93,9 @@ function addEventListenerToBtn() {
 
 function openModal(event) {
   if (event.currentTarget == event.target) return;
-  const dataForModal = restDataForModal(event);
-  modal.innerHTML = createMarkupModal(dataForModal);
+  modal.innerHTML = markup;
   modal.classList.remove('visually-hidden');
+  document.body.classList.add('stop-scrolling');
   addEventListenerToBtn();
 }
 
@@ -107,13 +106,16 @@ const modalItself = document.querySelector('.movie_card');
 function closeModalHandler(e) {
   if (e.code === 'Escape') {
     modal.classList.add('visually-hidden');
+    document.body.classList.remove('stop-scrolling');
   }
 }
 
 function closeBDModal(e) {
   if (e.target === e.currentTarget) {
     modal.classList.add('visually-hidden');
+    document.body.classList.remove('stop-scrolling');
   } else if (e.target.className === 'close-button') {
     modal.classList.add('visually-hidden');
+    document.body.classList.remove('stop-scrolling');
   } else return;
 }
--- src/partials_js/old.js
+++ src/partials_js/old.js
@@ -1,107 +1,108 @@
-import {
-  saveLocalStorageToWatched,
-  saveLocalStorageToQueue,
-} from './local_storage';
+// import {
+//   saveLocalStorageToWatched,
+//   saveLocalStorageToQueue,
+// } from './local_storage';
 
-const closeModalBtn = document.querySelector('#close-button-1');
-const divCard = document.querySelector('.js-modal-window');
-divCard.addEventListener('click', openModal);
+// const closeModalBtn = document.querySelector('#close-button-1');
+// const divCard = document.querySelector('.js-modal-window');
+// divCard.addEventListener('click', openModal);
 
-const modal = document.querySelector('.js-markup__modal');
+// const modal = document.querySelector('.js-markup__modal');
 
-const markup = `    <div class="movie-card" id="bright">
-        <div class="button-wrapper">
-            <div class="button-container">
-                <button class="close-button" id='close-button'>Close</button>
-            </div>
-            <div class="button-container">
-                <button class="watch-trailer js-watch-trailer" id='watch-trailer'>Watch Trailer</button>
-            </div>
-        </div>
-    
-        <div class="info-wrapper">
-    
-            <div class="#">
-                <img class="card-main-poster"
-                    src="https://movieplayer.net-cdn.it/t/images/2017/12/20/bright_jpg_191x283_crop_q85.jpg" />
-            </div>
-    
-            <div class="info-section">
-                <div class="title-wrapper">
-                    <h1 class="card-movie-title">Bright</h1>
-                </div>
-    
-                <table class="card-movie-info">
-                    <tr class="row">
-                        <td class="row-title">Vote / Votes</td>
-                        <td class="row-value"><span class="votes">7.3</span> / 1260</td>
-                    </tr>
-                    <tr class="row">
-                        <td class="row-title">Popularity</td>
-                        <td class="row-value">100.2</td>
-                    </tr>
-                    <tr class="row">
-                        <td class="row-title">Original Title</td>
-                        <td class="row-value row-title-original">
-                            fffffffff  fff</td>
-                    </tr>
-                    <tr class="row">
-                        <td class="row-title">Genre</td>
-                        <td class="row-value genres-row">Western Western Western Western Western Western Western Western</td>
-                    </tr>
-                </table>
-    
-                <div class="movie-descr">
-                    <p class="movie-descr__about">About</p>
-                    <p class="movie-descr__text">
-                        Four of the West’s most infamous outlaws assemble to steal a huge stash of gold from the most corrupt settlement of the gold rush towns. But not all goes to plan one is killed and the other three escapes with bags of gold hide out in the abandoned gold mine where they happen across another gang of three – who themselves were planning to hit the very same bank! As tensions rise, things go from bad to worse as they realise the bags of gold are filled with lead... they’ve been double crossed – but by who and how?
-                    </p>
-                </div>
-    
-                <div class="desktop-wrapper">
-                <ul class="modal-buttons">
-                    <li><button type="button" class="watch-btn js-btn-watched">Add to Watched</button></li>
-                    <li><button type="button" class="queue-btn js-btn-queue">Add to Queue</button></li>
-                </ul>
-    
-                </div>
-            </div>
-        </div>
-    
-    </div>`;
+// const markup = `<div class="movie-card" id="bright">
+//         <div class="button-wrapper">
+//             <div class="button-container">
+//                 <button class="close-button" id='close-button'>Close</button>
+//             </div>
+//             <div class="button-container">
+//                 <button class="watch-trailer js-watch-trailer" id='watch-trailer'>Watch Trailer</button>
+//             </div>
+//         </div>
 
-function addEventListenerToBtn() {
-  const watchBtn = modal.querySelector('.watch-btn');
-  const queueBtn = modal.querySelector('.queue-btn');
-  watchBtn.addEventListener('click', saveLocalStorageToWatched);
-  queueBtn.addEventListener('click', saveLocalStorageToQueue);
-}
+//         <div class="info-wrapper">
 
-function openModal(event) {
-  if (event.currentTarget == event.target) return;
-  modal.innerHTML = markup;
-  modal.classList.remove('visually-hidden');
-  document.body.classList.add('stop-scrolling');
-  addEventListenerToBtn();
-}
+//             <div class="#">
+//                 <img class="card-main-poster"
+//                     src="${BASE_URL_POSTER}${poster_path}" />
+//             </div>
 
-window.addEventListener('keydown', closeModalHandler);
-modal.addEventListener('click', closeBDModal);
-const modalItself = document.querySelector('.movie-card');
+//             <div class="info-section">
+//                 <div class="title-wrapper">
+//                     <h1 class="card-movie-title">${title}</h1>
+//                 </div>
 
-function closeModalHandler(e) {
-  if (e.code === 'Escape') {
-    modal.classList.add('visually-hidden');
-    document.body.classList.remove('stop-scrolling');
-  }
-}
+//                 <table class="card-movie-info">
+//                     <tr class="row">
+//                         <td class="row-title">Vote / Votes</td>
+//                         <td class="row-value"><span class="votes">${vote_average}</span> / ${vote_count}</td>
+//                     </tr>
+//                     <tr class="row">
+//                         <td class="row-title">Popularity</td>
+//                         <td class="row-value">${popularity}</td>
+//                     </tr>
+//                     <tr class="row">
+//                         <td class="row-title">Original Title</td>
+//                         <td class="row-value row-title-original">${original_title}</td>
+//                     </tr>
+//                     <tr class="row">
+//                         <td class="row-title">Genre</td>
+//                         <td class="row-value genres-row">${createStringOfGenres(
+//                           genre_ids
+//                         )}</td>
+//                     </tr>
+//                 </table>
 
-function closeBDModal(e) {
-  if (e.target === e.currentTarget) {
-    modal.classList.add('visually-hidden');
-    document.body.classList.remove('stop-scrolling');
-  } else if (e.target.className === 'close-button') {
-    modal.classList.add('visually-hidden');
-    document.body.classList.remove('stop-scrolling');
-  } else return;
-}
+//                 <div class="movie-descr">
+//                     <p class="movie-descr__about">About</p>
+//                     <p class="movie-descr__text">
+//                         ${overview}
+//                     </p>
+//                 </div>
+
+//                 <div class="desktop-wrapper">
+//                 <ul class="modal-buttons">
+//                     <li><button type="button" class="watch-btn js-btn-watched">Add to Watched</button></li>
+//                     <li><button type="button" class="queue-btn js-btn-queue">Add to Queue</button></li>
+//                 </ul>
+
+//                 </div>
+//             </div>
+//         </div>
+
+//     </div>`;
+
+// function addEventListenerToBtn() {
+//   const watchBtn = modal.querySelector('.watch-btn');
+//   const queueBtn = modal.querySelector('.queue-btn');
+//   watchBtn.addEventListener('click', saveLocalStorageToWatched);
+//   queueBtn.addEventListener('click', saveLocalStorageToQueue);
+// }
+
+// function openModal(event) {
+//   if (event.currentTarget == event.target) return;
+//   modal.innerHTML = markup;
+//   modal.classList.remove('visually-hidden');
+//   document.body.classList.add('stop-scrolling');
+//   addEventListenerToBtn();
+// }
+
+// window.addEventListener('keydown', closeModalHandler);
+// modal.addEventListener('click', closeBDModal);
+// const modalItself = document.querySelector('.movie-card');
+
+// function closeModalHandler(e) {
+//   if (e.code === 'Escape') {
+//     modal.classList.add('visually-hidden');
+//     document.body.classList.remove('stop-scrolling');
+//   }
+// }
+
+// function closeBDModal(e) {
+//   if (e.target === e.currentTarget) {
+//     modal.classList.add('visually-hidden');
+//     document.body.classList.remove('stop-scrolling');
+//   } else if (e.target.className === 'close-button') {
+//     modal.classList.add('visually-hidden');
+//     document.body.classList.remove('stop-scrolling');
+//   } else return;
+// }
