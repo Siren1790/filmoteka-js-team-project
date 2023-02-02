3e4f6cd36234936c8b5b5c6aba9a7fb2ca589736
Midland707 <rus14job@ukr.net>
2023-02-02 11:37:16 +0200

    add arrowKeysReactions

--- src/partials_js/refs.js
+++ src/partials_js/refs.js
@@ -4,8 +4,8 @@ const refs = {
   mainMarkFilms: document.querySelector('.list-films'),
   //pagination.js
   paginationWrapperNode: document.querySelector('.render-pagination-list'),
-  lastPaginationItemNode: document.querySelector('.last-page'),
-  firstPaginationItemNode: document.querySelector('.first-page'),
+  // lastPaginationItemNode: document.querySelector('.last-page'), //don`t use
+  // firstPaginationItemNode: document.querySelector('.first-page'), //don`t use
   prevPaginationNode: document.querySelector('.btn-arrow-prev'),
   nextPaginationNode: document.querySelector('.btn-arrow-next'),
   listPaginationNode: document.querySelector('.list-pagination'),
@@ -19,7 +19,7 @@ const refs = {
   //local_storage.js
   btnToWatched: document.querySelector('.js-btn-watched'),
   btnToQueue: document.querySelector('.js-btn-queue'),
-}
+};
 
 const refsApi = {
   API_KEY: 'fed7db976d902fcdece547680e82ff9e',
@@ -27,13 +27,12 @@ const refsApi = {
   API_URL_SEARCH_MOVIE: 'https://api.themoviedb.org/3/search/movie',
   API_URL_MOVIE_DETAILS: 'https://api.themoviedb.org/3/movie/',
   API_URL_MOVIE_GENRES: 'https://api.themoviedb.org/3/genre/movie/list',
-
-}
+};
 
 const refsStorage = {
   CURRENT_FILMS: 'currentFilms',
   STORAGE_KEY_WATCHED: 'addToWatched',
   STORAGE_KEY_QUEUE: 'addToQueue',
-}
+};
 
-export { refs, refsApi, refsStorage }
+export { refs, refsApi, refsStorage };
